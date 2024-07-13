c8a842f768f069588334c659c34ba0fe1b93837e
Mwakanemela <kayangemwakanemela@gmail.com>
2024-07-13 01:18:27 -0700

    loading an html tag

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const http = require("http");
 
 const server = http.createServer((request, response) => {
   response.statusCode = 200;
-  response.setHeader("Content-Type", "text/plain");
-  response.end("Welcome to my nodejs server");
+  response.setHeader("Content-Type", "text/html");
+  response.end("<h1>Welcome to my nodejs server</h1>");
 });
 
 server.listen(2024, "127.0.0.1", () => {
