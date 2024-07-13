ad6c3af6fd3b26f421438ed9a820b58976c721ac
Mwakanemela <kayangemwakanemela@gmail.com>
2024-07-13 07:09:55 -0700

    Intro to JSON (JavaScript Object Notation)

--- customers/src/app.js
+++ customers/src/app.js
@@ -2,8 +2,26 @@ const express = require("express");
 const app = express();
 const PORT = 2024;
 
+const customers = [
+  {
+    name: "JavaScript",
+    industry: "Frontend and Backend",
+  },
+  {
+    name: "HTML",
+    industry: "Frontend",
+  },
+  {
+    name: "CSS",
+    usedFor: "Styling",
+  },
+];
 app.get("/", (request, response) => {
   response.send("Welcome to my NodeJS Server(API) get...");
+});
+
+app.get("/api/customers", (req, res) => {
+  res.send({ customers: customers });
 });
 
 app.post("/", (request, response) => {
