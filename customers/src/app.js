77a8ade7b2acc52784b4bdde7635a02d723d1bd0
Mwakanemela <kayangemwakanemela@gmail.com>
2024-07-13 04:46:31 -0700

    Creating a get request endpoint

--- customers/src/app.js
+++ customers/src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 2024;
+
+app.get("/", (request, response) => {
+  response.send("Welcome to my NodeJS Server(API)...");
+});
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
