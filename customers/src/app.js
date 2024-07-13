241f87953a5c85e272c5e50ef3d9a9f788dc324f
Mwakanemela <kayangemwakanemela@gmail.com>
2024-07-13 05:10:50 -0700

    Applying(using) nodemon with express server

--- customers/src/app.js
+++ customers/src/app.js
@@ -3,7 +3,11 @@ const app = express();
 const PORT = 2024;
 
 app.get("/", (request, response) => {
-  response.send("Welcome to my NodeJS Server(API)...");
+  response.send("Welcome to my NodeJS Server(API) get...");
+});
+
+app.post("/", (request, response) => {
+  response.send("Welcome to NodeJS post request");
 });
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
